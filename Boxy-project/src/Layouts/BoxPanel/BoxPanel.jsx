25ceeefe0bbd60d729acd6bb54c73d62e31e0681
Mafoa <steve_mafoa@outlook.com>
2023-11-16 16:14:08 +0100

    Afficher les composants des propriétés de la box

--- Boxy-project/src/Layouts/BoxPanel/BoxPanel.jsx
+++ Boxy-project/src/Layouts/BoxPanel/BoxPanel.jsx
@@ -1,5 +1,22 @@
+import { useSelector } from "react-redux"
+import BoxRange from "./boxRange"
+import BoxColorPicker from "./BoxColorPicker"
+
 export default function BoxPanel() {
+  const boxState = useSelector(state => state.boxProperties)
+
+  const boxInputs = boxState.map((input, index) => {
+    if (input.type === "range") {
+      return <BoxRange key={index} inpuData={input} />
+    }
+    else if(input.type === "color") {
+      return <BoxColorPicker  key={index} inpuData={input}/>
+    }
+  })
   return (
-    <div>BoxPanel</div>
+    <div className=" bg-gray-50 px-6 py-4 border-b border-gray-300">
+      <p className="font-bold text-lg my-2">Box properties</p>
+      {boxInputs}
+    </div>
   )
 }

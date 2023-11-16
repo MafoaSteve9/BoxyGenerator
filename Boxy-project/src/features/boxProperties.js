d4e418b4637348016afd4440d99c877d84b050d5
Mafoa <steve_mafoa@outlook.com>
2023-11-16 17:00:29 +0100

    Update

---
+++ Boxy-project/src/features/boxProperties.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+    
+            {
+                inputNumber: 1,
+                name: "Border raduis",
+                value: 25,
+                type: "range",
+                slice: "boxProperties",
+                minMax: [0, 250]
+            },
+            {
+                inputNumber: 2,
+                name: "Height",
+                value: 250,
+                type: "range",
+                slice: "boxProperties",
+                minMax: [0, 500]
+            },
+            {
+                inputNumber: 3,
+                name: "Width",
+                value: 250,
+                type: "range",
+                slice: "boxProperties",
+                minMax: [0, 500]
+            },
+            {
+                inputNumber: 4,
+                name: "Background color",
+                value: "#fff",
+                slice: "boxProperties",
+                type: "color"
+            },
+        ]
+
+export const boxSlice = createSlice({
+    name: "boxProperties",
+    initialState,
+    reducers: {
+        updatBoxValue: (state, action) => {
+            state.find(el => el.inputNumber === action.payload.inputNumber).value = action.payload.value
+      }
+    },
+})
+
+export const {updatBoxValue} = boxSlice.actions
+export default boxSlice.reducer

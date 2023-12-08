e7b641c6a06551e4f4d3a604b03c2fb823c66925
Mafoa <steve_mafoa@outlook.com>
2023-12-08 03:00:12 +0100

    changement de nom de leftcontainer en creation

--- Boxy-project/src/App.jsx
+++ Boxy-project/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import LeftContainer from "./layouts/LeftContainer";
+import Creation from "./layouts/Creation";
 import Visualization from "./layouts/Visualization/Visualization";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap">
-        <LeftContainer />
+        <Creation />
         <Visualization />
       </main>
       <Footer />

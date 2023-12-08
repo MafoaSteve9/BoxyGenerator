467f39d309013fc8ef047f6dcfcb93b4be0f784c
Mafoa <steve_mafoa@outlook.com>
2023-12-08 03:06:30 +0100

    Modfication

--- Boxy-project/src/App.jsx
+++ Boxy-project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap">
-        <LeftContainer />
+        <Creation />
         <Visualization />
       </main>
       <Footer />
